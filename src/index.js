3f97f2202aa71bde2bd732ff2c266785c9551044
tzf1003 <1244201693@qq.com>
2026-09-06 20:53:29 +0800

    fix(conversation-tree): stream bound session snapshots

--- src/index.js
+++ src/index.js
@@ -17,3 +17,4 @@ export { graphModel, orderedNodeIds, queryMatches } from "./model.js";
 export { parseContext, navigationBlock } from "./context.js";
 export { canStartRead, isRequestCurrent, readResponsePatch } from "./controller-state.js";
 export { createController } from "./controller.js";
+export { ConversationTreeSnapshotReceiver } from "./snapshot-stream.js";
